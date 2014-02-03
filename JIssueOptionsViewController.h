fc9701d5810e6946edd91be492a1cd00e66fd466
maroon5five <randy_miller@mail.com>
2014-02-03 14:13:50 -0800

    added view controller for issue options

---
+++ JIssueOptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  JIssueOptionsViewController.h
+//  JIRAiOS
+//
+//  Created by Randy Miller on 2/3/14.
+//  Copyright (c) 2014 Randy Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JIssueOptionsViewController : UIViewController
+
+@end
---
+++ JIssueOptionsViewController.m
@@ -0,0 +1,38 @@
+//
+//  JIssueOptionsViewController.m
+//  JIRAiOS
+//
+//  Created by Randy Miller on 2/3/14.
+//  Copyright (c) 2014 Randy Miller. All rights reserved.
+//
+
+#import "JIssueOptionsViewController.h"
+
+@interface JIssueOptionsViewController ()
+
+@end
+
+@implementation JIssueOptionsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
